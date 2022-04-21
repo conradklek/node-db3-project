d62597f056839c5689ca73c640ef3a8c2278be92
GooieJS <conradklek@gmail.com>
2022-04-21 03:26:07 -0500

    push

--- api/schemes/scheme-middleware.js
+++ api/schemes/scheme-middleware.js
@@ -1,3 +1,4 @@
+const db = require("../../data/db-config");
 /*
   If `scheme_id` does not exist in the database:
 
@@ -6,8 +7,17 @@
     "message": "scheme with scheme_id <actual id> not found"
   }
 */
-const checkSchemeId = (req, res, next) => {
-
+const checkSchemeId = async (req, res, next) => {
+  try {
+    const existing = await db("schemes").where('scheme_id', req.params.scheme_id).first();
+    if (!existing) {
+      next({ status: 404, message: `scheme with scheme_id ${req.params.scheme_id} not found` });
+    } else {
+      next();
+    }
+  } catch (err) {
+    next(err)
+  }
 }
 
 /*
@@ -19,7 +29,10 @@ const checkSchemeId = (req, res, next) => {
   }
 */
 const validateScheme = (req, res, next) => {
-
+  if (!req.body.scheme_name || typeof req.body.scheme_name !== 'string' || !req.body.scheme_name.trim()) {
+    next({ status: 400, message: "invalid scheme_name" });
+  }
+  next();
 }
 
 /*
@@ -32,7 +45,12 @@ const validateScheme = (req, res, next) => {
   }
 */
 const validateStep = (req, res, next) => {
-
+  if (!req.body.instructions || typeof req.body.instructions !== 'string' || !req.body.instructions.trim()) {
+    next({ status: 400, message: "invalid step" });
+  } else if (!req.body.step_number || typeof req.body.step_number !== 'number' || req.body.step_number < 1) {
+    next({ status: 400, message: "invalid step" });
+  }
+  next();
 }
 
 module.exports = {
